d35fdeb8f495b573435ca108b621919c93070af5
besufikad <07yilmabese@gmail.com>
2022-10-15 11:51:38 +0300

    private

--- main.c
+++ main.c
@@ -1,36 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 
 
-  int merge(int a[] ,  int i ,int b[] , int j , int r[])
-{
-  if ( i == 0 && j == 0)
-        return 0;
 
-      if ( i ==0)
-      {
-          r[0] =b[0];
-          merge(a , i , b+1 , j-1 ,r+1);
-      }
-      if (j==0)
-      {
-          r[0] = a[0];
-          merge(a+1 ,i-1, b , j ,r+1);
+  int merge(int a[] ,  int i ,int b[] , int j , int r[]);
 
-      }
-      if (a[0] < b[0])
-      {
-          merge(a+1 , i-1 , b , j,r+1);
-      }
-      else
-      {
-          merge(a , i ,b+1 ,j-1 ,r+1);
-      }
-
-
-}
 int main()
 {
     int array1[] = {23,43,13,54,65,76,32,32,43};
@@ -40,6 +15,38 @@ int main()
     int b = 8;
      merge(array1 , a, array2 ,b ,result);
     for (int k=0;k<17;k++)
-        printf("%d" , merge(array1 , a, array2 ,b ,result));
+        printf("" , merge(array1 , a, array2 ,b ,result));
  return (0);
+}
+
+int merge(int a[] ,  int i ,int b[] , int j , int r[])
+{
+  if (i != 0 && j!=0)
+  {
+      if (j=0)
+      {
+          r[0] = a[0];
+          merge(a+1,i-1 , b  , j ,r+1);
+
+      }
+      if (i=0)
+      {
+          r[0] = b[0];
+          merge(a ,i , b+1 ,j-1,r+1);
+
+      }
+      else if (a[0] < b[0])
+      {
+          r[0] = a[0];
+          merge(a+1 , i-1 ,b , j ,r+1);
+      }
+      else
+      {
+          r[0] = b[0];
+          merge(a ,i , b+1 ,j-1 ,r+1);
+      }
+  }
+  else
+    return (0);
+
 }
--- main.h
+++ main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int merge(int a[] ,  int i ,int b[] , int j , int r[]) 
+
+void merge(int a[] ,  int i ,int b[] , int j , int r[])
 
 
 #endif /*MAIN_H*/
