983b431fc72dc62263b95c14985939180df86112
besufikad <07yilmabese@gmail.com>
2023-03-02 21:41:29 +0300

    late projects

--- checker.py
+++ checker.py
@@ -4,8 +4,9 @@
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
 replace_in_list = __import__('work2').replace_in_list
+
+new_list = replace_in_list(my_list, idx, new_element)
 
 print(new_list)
 print(my_list)
