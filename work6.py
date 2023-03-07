c2c1cc3af4d638ab82096e104a734bc4b7a56b2c
besufikad <07yilmabese@gmail.com>
2023-03-07 12:17:55 +0600

    late projects

---
+++ work6.py
@@ -0,0 +1,5 @@
+def multiple_returns(sentence):
+    if sentence == None or sentence == "":
+        return tuple(len(sentence) , sentence[0] = None)
+    else:
+        return tuple(len(sentence) , sentence[0])
