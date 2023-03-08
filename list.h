ec6ff6a59936c70da71c63a599fccae877902ca7
besufikad <07yilmabese@gmail.com>
2023-03-08 09:05:22 +0600

    linked lists

--- list.h
+++ list.h
@@ -6,9 +6,10 @@ typedef struct node{
     int x;
     struct node* next;
  }node;
-void  insert_end(node** root , int value);
-void insert_beginning(node** root , int value);
+  void insert_end(node** root , int value);
+  void insert_beginning(node** root , int value);
   void deallocate(node** root);
   void insert_middle(node* add , int value);
   void insert_sorted(node** root , int value);
+  void delete_list(node** root , int value);
   #endif/*LIST_H*/
--- workB.c
+++ workB.c
@@ -22,61 +22,64 @@
 
    insert_end(&root , 43);
    insert_end(&root , 4563);
+   insert_end(&root , 4563);
    insert_end(&root , 4576);
+   insert_middle(root->next , 3);
    insert_middle(root->next , 3);
    insert_beginning(&root , 201);
    insert_sorted(&root , 4567);
    insert_sorted(&root , 5000);
+   delete_list(&root , 4563);
 
 
 
 
    node* list = root;
    int i = 0;
-  for (list; list != NULL; list= list->next)
-  {
-    printf("%d\n" , list->x);
-    i++;
+   for (list; list != NULL; list= list->next)
+   {
+     printf("%d\n" , list->x);
+     i++;
+   }
+   deallocate(&root);
+   _CrtDumpMemoryLeaks();
+   printf("linked lists with %d values is added\n" , i);
+
+
+    return (0);
   }
-  deallocate(&root);
-  _CrtDumpMemoryLeaks();
- printf("linked lists with %d values is added\n" , i);
-
-
- return (0);
-  }
 
 
 
 
-   void  insert_end(node** root , int value)
-  {
-    node* new_node = malloc(sizeof(node));
-    if (new_node == NULL)
-    {
+ void  insert_end(node** root , int value)
+   {
+     node* new_node = malloc(sizeof(node));
+     if (new_node == NULL)
+     {
         exit(1);
-    }
-    new_node->x = value;
-    new_node->next = NULL;
+     }
+     new_node->x = value;
+     new_node->next = NULL;
 
 
-    node* curr = *root;
-    while(curr->next != NULL)
-    {
+     node* curr = *root;
+     while(curr->next != NULL)
+     {
         curr = curr->next;
-    }
-    curr->next = new_node;
-  }
-  void deallocate(node** root)
+     }
+      curr->next = new_node;
+   }
+ void deallocate(node** root)
   {
    node* curr = *root;
    while(curr != NULL)
    {
-    node* deal = curr;
-    curr = curr->next;
-    free(deal);
+     node* deal = curr;
+     curr = curr->next;
+     free(deal);
    }
- *root = NULL;
+   *root = NULL;
 
   }
   void insert_beginning(node** root , int value)
@@ -104,7 +107,7 @@
   {
     if (*root == NULL ||(*root)->x > value)
     {
-      insert_beginning(&root , value);
+      insert_beginning(root , value);
       return;
     }
     node* curr = * root;
@@ -118,4 +121,27 @@
       curr = curr->next;
     }
     insert_middle(curr , value);
+  }
+  void delete_list(node** root , int value)
+  {
+    if (*root == NULL)
+    {
+      return;
+    }
+     
+     for(node* curr = *root; curr->next != NULL; curr = curr->next)
+     {
+      
+      
+        do
+          {
+           node* removed = curr->next;
+           curr->next = curr->next->next;
+           free(removed);
+          }while(curr->next->x == value);
+      
+     }
+
+         
+
   }
